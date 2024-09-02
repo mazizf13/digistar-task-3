2bd0cb378ee9d7631903cfa36ed4ca5e10fa6f2a
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-09-02 21:35:22 +0700

    feat: implement Axios to fetch and display dynamic content in Media component

--- src/components/card/CardMedia.jsx
+++ src/components/card/CardMedia.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import { useDarkMode } from "../../contexts/DarkModeContext";
 
-const CardMedia = ({ cardImg, cardTitle, cardDescription }) => {
+const CardMedia = ({ cardId, cardImg, cardTitle, cardDescription }) => {
   const { isDarkMode } = useDarkMode();
 
   return (
-    <div className="md:mb-0 mb-6">
+    <div className="md:mb-6 mb-4 p-4">
       <section className="flex relative w-full h-full">
         <a
-          className={`space-y-6 appearance-none mr-5 flex flex-col w-full h-full ${isDarkMode ? "text-white" : ""}`}
-          type="button"
-          name="article-link"
-          href="https://info.padiumkm.id/media/mengenal-jenis-tripod-dan-masing-masing-kelebihannya"
+          className={`space-y-4 appearance-none flex flex-col w-full h-full ${isDarkMode ? "text-white" : "text-gray-900"}`}
+          href={`https://jsonplaceholder.typicode.com/posts/${cardId}`}
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <figure
-            className={`lg:flex lg:mx-auto w-full relative overflow-hidden rounded-lg lg:text-center self-center px-0 ${isDarkMode ? "bg-gray-800" : "bg-gray-100"}`}
+            className={`w-full relative overflow-hidden rounded-lg self-center ${isDarkMode ? "bg-gray-800" : "bg-gray-100"}`}
           >
             <img
-              alt="3-rekomendasi-handphone-yang-bisa-anda-beli-dengan-spesifikasi-mumpuni"
-              className="rounded-md"
+              alt={cardTitle}
+              className="rounded-md w-full object-cover"
               src={cardImg}
               sizes="100vw"
               data-nimg="fill"
             />
           </figure>
-          <section className="space-y-2">
+          <section className="space-y-2 mt-4">
             <h3
-              className={`min-h-14 text-xl font-bold line-clamp-2 ${isDarkMode ? "text-white" : ""}`}
+              className={`text-xl font-bold line-clamp-2 ${isDarkMode ? "text-white" : "text-gray-900"}`}
             >
               {cardTitle}
             </h3>
             <article
-              className={`text-gray-400 min-h-24 w-full text-justify ${isDarkMode ? "text-gray-300" : ""}`}
+              className={`min-h-24 w-full text-justify ${isDarkMode ? "text-gray-300" : "text-gray-600"}`}
             >
               <div className="line-clamp-4">{cardDescription}</div>
             </article>
--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -1,52 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/layout/Layout";
-import { media } from "../data/item";
 import Card from "../components/card/CardMedia";
 import { useDarkMode } from "../contexts/DarkModeContext";
+import axios from "axios";
 
 const Media = () => {
+  const [articles, setArticles] = useState([]);
+  const [visibleArticles, setVisibleArticles] = useState(6);
+  const [isLoading, setIsLoading] = useState(true);
   const { isDarkMode } = useDarkMode();
 
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts",
+        );
+        setArticles(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+        setIsLoading(false);
+      }
+    };
+    fetchArticles();
+  }, []);
+
+  const loadMore = () => {
+    setVisibleArticles((prevVisibleArticles) => prevVisibleArticles + 6);
+  };
+
   return (
-    <>
-      <Layout>
+    <Layout>
+      <div className="flex md:container md:mx-auto justify-between lg:px-40 ">
         <div
-          className={`flex md:container md:mx-auto justify-between lg:px-40 ${isDarkMode ? "bg-gray-900" : "bg-white"}`}
+          className={`m-0 p-4 xl:p-0 xl:pr-8 w-full mb-10 ${isDarkMode ? "text-gray-300" : "text-gray-900"}`}
         >
-          <div
-            className={`m-0 p-4 xl:p-0 xl:pr-8 w-full mb-10 ${isDarkMode ? "text-gray-300" : "text-gray-900"}`}
-          >
-            <div className={`py-4 `}>
-              <div className="flex-grow flex-col px-5 md:py-20 py-6 space-y-8">
-                <section className="flex flex-col mb-4">
-                  <section className="w-fit">
-                    <div
-                      className={`text-center flex justify-center rounded-full border border-b-4 ${isDarkMode ? "border-[#4DB3C5]" : "border-[#4DB3C5]"} w-[80px] mb-2 mx-auto`}
-                    />
-                    <h2
-                      className={`w-fit text-3xl font-bold ${isDarkMode ? "text-gray-100" : "text-[#0E0F11]"}`}
-                    >
-                      {media.title}
-                    </h2>
-                  </section>
-                  <p className="mt-2">{media.desc}</p>
+          <div className="py-4">
+            <div className="flex-grow flex-col px-5 md:py-20 py-6 space-y-8">
+              <section className="flex flex-col mb-4">
+                <section className="w-fit">
+                  <div
+                    className={`text-center flex justify-center rounded-full border border-b-4 w-[80px] mb-2 mx-auto ${isDarkMode ? "border-[#4DB3C5]" : "border-[#4DB3C5]"}`}
+                  />
+                  <h2
+                    className={`w-fit text-3xl font-bold ${isDarkMode ? "text-gray-100" : "text-[#0E0F11]"}`}
+                  >
+                    Rekomendasi
+                  </h2>
                 </section>
-                <section className="md:grid md:grid-cols-3 sm:flex sm:flex-row w-full h-full justify-self-stretch">
-                  {media.articels.map((article, index) => (
-                    <Card
-                      key={index}
-                      cardTitle={article.title}
-                      cardImg={article.img}
-                      cardDescription={article.detail}
-                    />
-                  ))}
-                </section>
-              </div>
+                <p
+                  className={`mt-2 ${isDarkMode ? "text-gray-400" : "text-gray-900"}`}
+                >
+                  Rekomendasi produk sesuai kebutuhan pembeli
+                </p>
+              </section>
+              <section className="md:grid md:grid-cols-3 sm:flex sm:flex-row w-full h-full justify-self-stretch">
+                {isLoading ? (
+                  <p>Loading...</p>
+                ) : (
+                  articles
+                    .slice(0, visibleArticles)
+                    .map((article, index) => (
+                      <Card
+                        key={index}
+                        cardId={article.id}
+                        cardTitle={article.title}
+                        cardImg={`https://picsum.photos/id/${index}/360/300`}
+                        cardDescription={article.body}
+                      />
+                    ))
+                )}
+              </section>
+              <button
+                onClick={loadMore}
+                disabled={visibleArticles >= articles.length}
+                className="mt-6 bg-[#009EA9] text-white px-4 py-2 rounded disabled:opacity-50 w-full hover:bg-[#50bcc4]"
+              >
+                Load More
+              </button>
             </div>
           </div>
         </div>
-      </Layout>
-    </>
+      </div>
+    </Layout>
   );
 };
 
